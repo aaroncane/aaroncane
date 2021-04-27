694c09cd3147922e98d8f2f7acce16a79d154d2a
aaroncane <70538090+aaroncane@users.noreply.github.com>
2021-04-27 17:15:35 -0500

    Add files via upload

---
+++ datos.php
@@ -0,0 +1,27 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chismografo</title>
+</head>
+
+<body style="background-color: rgba(255, 195, 29, 0.952); text-align: center;">
+<?php
+$n=$_POST['nombre'];
+echo "Gracias".$n. "por contestar el chismografo";
+
+?>
+
+    <h1>Dale clic en paty chapoy para regresar</h1>
+    <a href="index.html">
+        <img src="https://www.elsoldemexico.com.mx/gossip/omg/jxcnbq-patychapoy.png/ALTERNATES/LANDSCAPE_1140/patychapoy.png"
+            alt="Paty chapoy decepcionada">
+    </a>
+</body>
+
+</html>-->
